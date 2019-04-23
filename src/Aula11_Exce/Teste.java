beacfc02dc5bb746a59606694d3e4386e74c1504
xfarsx <fernando.arsantos@gmail.com>
2019-04-22 22:15:05 -0300

    Initial commit

---
+++ src/Aula11_Exce/Teste.java
@@ -0,0 +1,14 @@
+package Aula11_Exce;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        CalculoMatematico teste = new CalculoMatematico (4,0);
+
+        teste.divisao();
+
+        try{teste.divisao2();}catch(ArithmeticException e){
+            System.out.println(e.getMessage());}
+    }
+}
+
